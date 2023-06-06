ac848e00f8fe9441a8b2dacff512a0c5cca373d8
Maxbwee <maxbowen1234@gmail.com>
2023-06-06 14:05:54 +0300

    Working version of calculation and error handling. Also added styling

---
+++ src/components/Calculator/Operators.js
@@ -0,0 +1,32 @@
+
+  /*   const handleEqualClick = () => {
+        try {
+          const tokens = input.split(' '); // Split the input by spaces
+          let result = parseFloat(tokens[0]);
+      
+          for (let i = 1; i < tokens.length; i += 2) {
+            const operator = tokens[i];
+            const operand = parseFloat(tokens[i + 1]);
+      
+            switch (operator) {
+              case '+':
+                result += operand;
+                break;
+              case '-':
+                result -= operand;
+                break;
+              case '*':
+                result *= operand;
+                break;
+              case '/':
+                result /= operand;
+                break;
+              // Handle other operators as needed
+            }
+          }
+      
+          setInput(result.toString());
+        } catch (error) {
+          setInput('Error');
+        }
+      }; */
--- src/pages/Calculator.js
+++ src/pages/Calculator.js
@@ -1,116 +1,195 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
+import React , {useState} from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import { Grid } from '@mui/material';
+import { Grid, TextField } from '@mui/material';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import "../styles/Styles.css"; // import the style.css file
+import * as math from 'mathjs';
 
 
 export default function Calculator() {
+
+    // Used as the users inputted numbers
+    const [input, setInput] = useState(''); 
+
+    // Handles users click of the buttons
+    const handleButtonClick = (value) => {
+        setInput((prevInput) => prevInput + value)
+    };
+
+    // Specific button to handle clearing numbers
+    const handleClearClick = () => {
+        setInput('');
+    }
+
+    // Specific button to perform the calculation
+    const handleEqualClick = () => {
+        try {
+            const result = math.evaluate(input);
+            setInput(result.toString());
+        } catch (error) {
+            setInput('Error')
+        }
+    }
+
     return(
       <div className="cardContainer">
-        <Card className="customCard">
+        <Card className="customCard"
+        style={{backgroundColor: '#F4DFD0'}}
+        >
             <CardContent>
-            <Typography sx={{ fontSize: 14 }}> Result: </Typography>
-
-            <Grid  container spacing={2} justifyContent="center" alignItems="center">
-            <Grid item xs={4}>
+            <Typography style={{ fontSize: 14, marginBottom: 10 }}> Calculator </Typography>
+            <TextField
+                label="Result"
+                variant="outlined"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                fullWidth
+            />
+            <Grid  container spacing={3} justifyContent="center" alignItems="center" marginTop={2}>
+            <Grid item xs={3}>
                 <Button
                 variant="contained"
-                className="customButton"
+                style={{ backgroundColor: '#E3CAA5', color: 'black'}}
+                onClick={() => handleButtonClick('1')}
                 > 
                 1
                 </Button>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
             <Button
                 variant="contained"
-                className="customButton"
+                style={{ backgroundColor: '#E3CAA5', color: 'black'}}
+                onClick={() => handleButtonClick('2')}
                 > 
                 2
                 </Button>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
             <Button
                 variant="contained"
-                className="customButton"
+                style={{ backgroundColor: '#E3CAA5', color: 'black'}}
+                onClick={() => handleButtonClick('3')}
                 > 
                 3
                 </Button>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
             <Button
                 variant="contained"
-                className="customButton"
+                style={{ backgroundColor: '#E3CAA5', color: 'black'}}
+                onClick={() => handleButtonClick('+')}
+                > 
+                +
+                </Button>
+            </Grid>
+            <Grid item xs={3}>
+            <Button
+                variant="contained"
+                style={{ backgroundColor: '#E3CAA5', color: 'black'}}
+                onClick={() => handleButtonClick('4')}
                 > 
                 4
                 </Button>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
             <Button
                 variant="contained"
-                className="customButton"
+                style={{ backgroundColor: '#E3CAA5', color: 'black'}}
+                onClick={() => handleButtonClick('5')}
                 > 
                 5
                 </Button>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
             <Button
                 variant="contained"
-                className="customButton"
+                style={{ backgroundColor: '#E3CAA5', color: 'black'}}
+                onClick={() => handleButtonClick('6')}
                 > 
                 6
                 </Button>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
             <Button
                 variant="contained"
-                className="customButton"
+                style={{ backgroundColor: '#E3CAA5', color: 'black'}}
+                onClick={() => handleButtonClick('-')}
+                > 
+                -
+                </Button>
+            </Grid>
+            <Grid item xs={3}>
+            <Button
+                variant="contained"
+                style={{ backgroundColor: '#E3CAA5', color: 'black'}}
+                onClick={() => handleButtonClick('7')}
                 > 
                 7
                 </Button>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
             <Button
                 variant="contained"
-                className="customButton"
+                style={{ backgroundColor: '#E3CAA5', color: 'black'}}
+                onClick={() => handleButtonClick('8')}
                 > 
                 8
                 </Button>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
             <Button
                 variant="contained"
-                className="customButton"
+                style={{ backgroundColor: '#E3CAA5', color: 'black'}}
+                onClick={() => handleButtonClick('9')}
                 > 
                 9
                 </Button>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
             <Button
                 variant="contained"
-                className="customButton"
+                style={{ backgroundColor: '#E3CAA5', color: 'black'}}
+                onClick={() => handleButtonClick('*')}
+                > 
+                *
+                </Button>
+            </Grid>
+            <Grid item xs={3}>
+            <Button
+                variant="contained"
+                style={{ backgroundColor: '#E3CAA5', color: 'black'}}
+                onClick={() => handleClearClick()}
                 > 
                 C
                 </Button>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
             <Button
                 variant="contained"
-                className="customButton"
+                style={{ backgroundColor: '#E3CAA5', color: 'black'}}
+                onClick={() => handleButtonClick('0')}
                 > 
                 0
                 </Button>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={3}>
             <Button
                 variant="contained"
-                className="customButton"
+                style={{ backgroundColor: '#E3CAA5', color: 'black'}}
+                onClick={() => handleEqualClick()}
                 > 
                 =
+                </Button>
+            </Grid>
+            <Grid item xs={3}>
+            <Button
+                variant="contained"
+                style={{ backgroundColor: '#E3CAA5', color: 'black'}}
+                onClick={() => handleButtonClick('/')}
+                > 
+                /
                 </Button>
             </Grid>
             </Grid>
